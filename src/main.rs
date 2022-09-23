28d5becbbf3b1f983f589c731ce29aaaaa9d385c
Tobias Prisching <tobias.prisching@icloud.com>
2022-09-24 01:49:33 +0200

    Managed to create a new application with a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::Application;
+use gtk::ApplicationWindow;
+
+const APP_ID: &str = "org.tp.techniktobi.luixpreview";
+
+fn
+main
+() 
+{
+	
+	// Create a new application
+	// This uses the builder pattern,
+	// which is supported by many gtk-rs objects
+	let app = Application::builder()
+		.application_id(APP_ID)
+		.build();
+
+	// Connect the build_ui function to the "activate" signal of "app"
+	app.connect_activate(build_ui);
+
+	// Run the application
+	app.run();
+}
+
+fn
+build_ui
+(
+	app: &Application
+)
+{
+	// Create a new window & set its title
+	let window = ApplicationWindow::builder()
+		.application(app)
+		.title("LuixPreview")
+		.build();
+	
+	// Present the window
+	window.present();
 }
