2bc0451ff1e56dc349a428365a4d61e91bef5737
Tobias Prisching <tobias.prisching@icloud.com>
2022-10-10 05:21:38 +0200

    Started implementing trait for the application state struct
    Using rust-analyzer, a VS code extension, this was really easy!

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,33 @@ ECounterMessage
 	Decrement
 }
 
+impl Sandbox for Counter
+{
+	// A type alias to the Message type we will use
+    type Message = ();
+
+	// Initialises the state of the application
+    fn new() -> Self {
+        todo!()
+    }
+
+	// Sets the title of the application
+    fn title(&self) -> String {
+        todo!()
+    }
+
+	// Receives messages of the above defined type and changes the applications state
+    fn update(&mut self, message: Self::Message) {
+        todo!()
+    }
+
+	// Draws the GUI
+	// Returns an iced element that occupies the view
+    fn view(&self) -> iced::pure::Element<'_, Self::Message> {
+        todo!()
+    }
+}
+
 fn
 main
 () 
