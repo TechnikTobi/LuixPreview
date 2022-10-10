c52f85537e354860cec6ddd2f8b1e7e6841f6b27
Tobias Prisching <tobias.prisching@icloud.com>
2022-10-10 08:45:55 +0200

    First successful application

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use iced::Settings;
 use iced::pure::Sandbox;
 use iced::pure::widget::{Button, Text, Column, Container};
 
-const APP_ID: &str = "org.tp.techniktobi.luixpreview";
+// const APP_ID: &str = "org.tp.techniktobi.luixpreview";
 
 struct 
 Counter
@@ -10,6 +10,7 @@ Counter
 	count: i64
 }
 
+#[derive(Debug, Clone, Copy)]
 enum
 ECounterMessage
 {
@@ -20,27 +21,52 @@ ECounterMessage
 impl Sandbox for Counter
 {
 	// A type alias to the Message type we will use
-    type Message = ();
+    type Message = ECounterMessage;
 
 	// Initialises the state of the application
     fn new() -> Self {
-        todo!()
+        Counter 
+        { 
+            count: 0 
+        }
     }
 
 	// Sets the title of the application
     fn title(&self) -> String {
-        todo!()
+        String::from("A Counter Application written in Rust using Iced")
     }
 
 	// Receives messages of the above defined type and changes the applications state
     fn update(&mut self, message: Self::Message) {
-        todo!()
+        match message
+        {
+            ECounterMessage::Increment => self.count += 1,
+            ECounterMessage::Decrement => self.count -= 1,
+        }
     }
 
 	// Draws the GUI
 	// Returns an iced element that occupies the view
     fn view(&self) -> iced::pure::Element<'_, Self::Message> {
-        todo!()
+
+        // A label for displaying the currrent count
+        let label = Text::new(format!("Count: {}", self.count));
+
+        // Buttons for messages
+        let button_increment = Button::new("Increment").on_press(ECounterMessage::Increment);
+        let button_decrement = Button::new("Decrement").on_press(ECounterMessage::Decrement);
+
+        let column = Column::new()
+            .push(label)
+            .push(button_increment)
+            .push(button_decrement);
+
+        return Container::new(column)
+            .center_x()
+            .center_y()
+            .width(iced::Length::Fill)
+            .height(iced::Length::Fill)
+            .into()
     }
 }
 
@@ -49,6 +75,6 @@ main
 () 
 {
 
-	println!("Hello World!");
+	Counter::run(Settings::default());
 
 }	
